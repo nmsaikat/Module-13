cd6d86d5e4160b5010f9f966b38d6adc29e10848
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-07 18:03:44 +0600

    Pass Data From Controller

--- app2/app/Http/Controllers/HomeController.php
+++ app2/app/Http/Controllers/HomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function page(){
-        return view('home');
+    function page(Request $request){
+
+        $num1 = $request -> num1;
+        $num2 = $request -> num2;
+        $sum = $num1 + $num2;
+
+        $data = ['result' => $sum];
+        return view('home', $data);
     }
 }
