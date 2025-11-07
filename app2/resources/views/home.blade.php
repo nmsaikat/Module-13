4efea6fd41d585b76ce1c505412ff702f6f69afb
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-07 18:16:10 +0600

    Blade if else

--- app2/resources/views/home.blade.php
+++ app2/resources/views/home.blade.php
@@ -1,1 +1,11 @@
-<h2>{{ $result }}</h2>
+<!-- <h2>{{ $result }}</h2> -->
+
+@if ($result == 100)
+    <h2>Result is 100</h2>
+
+@elseif( $result == 200 )
+    <h2>Result is 200</h2>
+@else
+    <h2>Result is not in our range</h2>
+
+@endif
