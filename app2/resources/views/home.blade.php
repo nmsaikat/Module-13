5c1d56804f029cdb20ce01f007442fa1b10380bb
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-08 12:49:38 +0600

    Blade Including Asssets

--- app2/resources/views/home.blade.php
+++ app2/resources/views/home.blade.php
@@ -1,8 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
 <body>
-    <ul>
-        @foreach ($users as $user)
-            <li>User Name Is {{ $user ['name'] }} And City Is {{ $user ['city'] }}</li>
-        @endforeach
-    </ul>
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <button class="btn btn-success">Hello</button>
+                <img src="{{ assert('images/laravel.jpeg') }}" alt="">
+            </div>
+        </div>
+    </div>
+    
+
+    <script src="{{ assert('js/bootstrap.bundle.js') }}"></script>
 </body>
+</html>
